f2bda43c31c40d90308277a5b3ec81df02bef714
Linnea Toré <linneatore@Linneas-MacBook-Pro.local>
2020-01-09 09:36:02 +0100

    installerat allt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import io from 'socket.io-client';
 import './App.css';
+
+const socket = io('http://3.120.96.16:3000/');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MAKE CHATT</h1>
     </div>
   );
 }
